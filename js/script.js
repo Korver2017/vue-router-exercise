cf07c61cdf12074af0f21be7afcef70735994dfa
Korver <korver2017@gmail.com>
2018-07-28 01:10:40 +0800

    build List component

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
+let List = {
+  template: `
+    <div>
+      <p>
+        <input type="text" v-model.trim="input">
+        <button @click="create"></button>
+      </p>
+      <ol>
+        <li v-for="(item, index) in contents" :key="item.id">
+          {{ item.content }}
+        </li>
+      </ol>
+    </div>`
+};
+
+let Edit = {
+
+}
+
 let router = new VueRouter({
   routes: [{
       path: '/',
